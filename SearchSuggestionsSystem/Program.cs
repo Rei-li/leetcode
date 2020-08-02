019738dd9ad4fc4582eb26f38d2989abfad24d86
rei-li <rei-li@mail.ru>
2020-08-02 23:59:23 +0300

    leetcode https://leetcode.com/problems/search-suggestions-system/

---
+++ SearchSuggestionsSystem/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchSuggestionsSystem
+{
+    /// <summary>
+    /// leetcode https://leetcode.com/problems/search-suggestions-system/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            var s = new Solution();
+            var products = new string[1] { "havana" };
+            var results = s.SuggestedProducts(products, "tatiana");
+
+            Console.WriteLine("Results:");
+            Console.WriteLine(results.ToString());
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Solution
+    {
+        public class Node
+        {
+            public Dictionary<char, Node> dict = new Dictionary<char, Node>();
+            public string word;
+            public LinkedList<char> orderedKeys = new LinkedList<char>();
+        }
+
+        public void AddBrunch(string word, Node node)
+        {
+            var lastNode = node;
+
+            foreach (char letter in word)
+            {
+                lastNode = AddNode(letter, lastNode);
+            }
+
+            lastNode.word = word;
+        }
+
+        public Node AddNode(char letter, Node node)
+        {
+            var nextLevelNode = new Node();
+            if (!node.orderedKeys.Contains(letter))
+            {
+                node.dict.Add(letter, nextLevelNode);
+
+                if (node.orderedKeys.Any())
+                {
+                    var lastKey = node.orderedKeys.Last;
+                    if (lastKey.Value < letter)
+                    {
+                        node.orderedKeys.AddAfter(lastKey, letter);
+                    }
+                    else
+                    {
+                        foreach (var key in node.orderedKeys)
+                        {
+                            if (letter < key)
+                            {
+                                node.orderedKeys.AddBefore(node.orderedKeys.Find(key), letter);
+                                break;
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    node.orderedKeys.AddFirst(letter);
+                }
+            }
+            else
+            {
+                node.dict.TryGetValue(letter, out nextLevelNode);
+            }
+
+            return nextLevelNode;
+        }
+
+        public Node BuildIndex(string[] products)
+        {
+            var root = new Node();
+            foreach (var prod in products)
+            {
+                AddBrunch(prod, root);
+            }
+
+            return root;
+        }
+
+        public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            Node selectedNode = BuildIndex(products);
+            var results = new List<IList<string>>();
+
+            foreach (var letter in searchWord)
+            {
+                List<string> letterResults = new List<string>();
+                if (selectedNode != null)
+                {
+                    selectedNode.dict.TryGetValue(letter, out selectedNode);
+
+                    if (selectedNode != null)
+                    {
+                        LetterSearch(selectedNode, letterResults);
+
+                    }
+                }
+                results.Add(letterResults);
+            }
+
+            return results;
+        }
+
+        public void LetterSearch(Node node, IList<string> letterResults)
+        {
+            if (node.word != null)
+            {
+                letterResults.Add(node.word);
+            }
+
+            if (node.orderedKeys.Any())
+            {
+                foreach (var key in node.orderedKeys)
+                {
+                    if (letterResults.Count >= 3)
+                    {
+                        break;
+                    }
+
+                    var selectedNode = new Node();
+                    node.dict.TryGetValue(key, out selectedNode);
+                    LetterSearch(selectedNode, letterResults);
+                }
+            }
+        }
+    }
+}
--- SearchSuggestionsSystem/Properties/AssemblyInfo.cs
+++ SearchSuggestionsSystem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("leetcode")]
+[assembly: AssemblyTitle("SearchSuggestionsSystem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("leetcode")]
+[assembly: AssemblyProduct("SearchSuggestionsSystem")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4b20f2eb-0266-419c-9bd3-5e91ec43e93a")]
+[assembly: Guid("df9d89ae-a59a-49e5-97e2-9e7769cb5b37")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
