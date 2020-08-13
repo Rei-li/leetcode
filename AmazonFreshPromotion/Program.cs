affac62993c24d441d11da29be4ca990add1108f
rei-li <rei-li@mail.ru>
2020-08-14 00:01:15 +0300

    Top K Frequent Words

---
+++ AmazonFreshPromotion/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonFreshPromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start1");
+            var shoppingCart = new List<string> { "orange", "apple", "apple", "banana", "orange", "banana" };
+            var codeList = new List<List<string>>();
+            codeList.Add(new List<string> { "apple", "apple" });
+            codeList.Add(new List<string> { "banana", "anything", "banana" });
+
+            foreach (var prod in shoppingCart)
+            {
+                Console.WriteLine(prod);
+            }
+
+            foreach (var combo in codeList)
+            {
+                foreach (var prod in combo)
+                {
+                    Console.Write(prod + " ");
+                }
+                Console.WriteLine();
+            }
+
+            var result = CheckWinner(codeList, shoppingCart);
+
+            Console.WriteLine();
+            Console.WriteLine("Result: " + result);
+
+
+            Console.WriteLine("Start2");
+            shoppingCart = new List<string> { "banana", "orange", "banana", "apple", "apple" };
+            codeList = new List<List<string>>();
+            codeList.Add(new List<string> { "apple", "apple" });
+            codeList.Add(new List<string> { "banana", "anything", "banana" });
+
+            foreach (var prod in shoppingCart)
+            {
+                Console.WriteLine(prod);
+            }
+
+            foreach (var combo in codeList)
+            {
+                foreach (var prod in combo)
+                {
+                    Console.Write(prod + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result: " + CheckWinner(codeList, shoppingCart));
+
+
+            Console.WriteLine("Start3");
+            shoppingCart = new List<string> { "apple", "banana", "apple", "banana", "orange", "banana" };
+            codeList = new List<List<string>>();
+            codeList.Add(new List<string> { "apple", "apple" });
+            codeList.Add(new List<string> { "banana", "anything", "banana" });
+
+            foreach (var prod in shoppingCart)
+            {
+                Console.WriteLine(prod);
+            }
+
+            foreach (var combo in codeList)
+            {
+                foreach (var prod in combo)
+                {
+                    Console.Write(prod + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result: " + CheckWinner(codeList, shoppingCart));
+
+
+            Console.WriteLine("Start4");
+            shoppingCart = new List<string> { "apple", "apple", "apple", "banana" };
+            codeList = new List<List<string>>();
+            codeList.Add(new List<string> { "apple", "apple" });
+            codeList.Add(new List<string> { "banana", "apple", "banana" });
+
+            foreach (var prod in shoppingCart)
+            {
+                Console.WriteLine(prod);
+            }
+
+            foreach (var combo in codeList)
+            {
+                foreach (var prod in combo)
+                {
+                    Console.Write(prod + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Result: " + CheckWinner(codeList, shoppingCart));
+
+            Console.ReadKey();
+        }
+
+        public static int CheckWinner(List<List<string>> codeList, IList<string> shoppingCart)
+        {
+            if (!codeList.Any())
+            {
+                return 1;
+            }
+
+            if (!shoppingCart.Any())
+            {
+                return 0;
+            }
+
+            int codeListCombinationPositin = 0;
+            int codeListLevelPositin = 0;
+
+            foreach (var prod in shoppingCart)
+            {
+                var isEqual = CheckIfProdFitPosition(prod, codeList[codeListLevelPositin][codeListCombinationPositin]);
+                if (isEqual)
+                {
+                    var positions = MoveCodeListCounters(codeList, codeListCombinationPositin, codeListLevelPositin);
+                    codeListCombinationPositin = positions.codeListCombinationPositin;
+                    codeListLevelPositin = positions.codeListLevelPositin;
+
+                    if (codeListLevelPositin >= codeList.Count)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    codeListCombinationPositin = 0;
+                    if (CheckIfProdFitPosition(prod, codeList[codeListLevelPositin][codeListCombinationPositin]))
+                    {
+                        var positions = MoveCodeListCounters(codeList, codeListCombinationPositin, codeListLevelPositin);
+                        codeListCombinationPositin = positions.codeListCombinationPositin;
+                        codeListLevelPositin = positions.codeListLevelPositin;
+                    }
+
+                }
+            }
+
+            if (codeListLevelPositin + 1 == codeList.Count && codeListCombinationPositin + 1 == codeList[codeListLevelPositin].Count)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool CheckIfProdFitPosition(string prod, string codeListProd)
+        {
+            if (string.Equals(codeListProd, "anything"))
+            {
+                return true;
+            }
+
+            return string.Equals(prod, codeListProd);
+        }
+
+        public static (int codeListCombinationPositin, int codeListLevelPositin) MoveCodeListCounters(
+            List<List<string>> codeList,
+            int codeListCombinationPositin,
+            int codeListLevelPositin)
+        {
+            if (codeListCombinationPositin + 1 < codeList[codeListLevelPositin].Count)
+            {
+                codeListCombinationPositin++;
+            }
+            else if (codeListLevelPositin + 1 < codeList.Count)
+            {
+                codeListCombinationPositin = 0;
+                codeListLevelPositin++;
+
+            }
+
+            return (codeListCombinationPositin, codeListLevelPositin);
+        }
+    }
+}
---
+++ TopKFrequentWords/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopKFrequentWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            var solution = new Solution();
+            var results = solution.TopKFrequent(new string[] { "i", "love", "leetcode", "i", "love", "coding" }, 2);
+            foreach (var word in results)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("Start2");
+            var results2 = solution.TopKFrequent(new string[] { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" }, 4);
+            foreach (var word in results2)
+            {
+                Console.WriteLine(word);
+            }
+
+
+            Console.ReadKey();
+        }         
+    }
+
+    public class Solution
+    {
+        public IList<string> TopKFrequent(string[] words, int k)
+        {
+            Dictionary<string, int> countedWords = CountWordsEntries(words);
+            List<KeyValuePair<string, int>>[] bucketsList = ThrowToBucket(countedWords, words);
+
+            var results = new List<string>();
+
+            for(int i = bucketsList.Count()-1; i >= 0; i--)
+            {
+                if (results.Count >= k)
+                {
+                    break;
+                }
+
+                var bucket = bucketsList[i];
+                if (bucket != null && bucket.Any())
+                {
+                    var orderedBucket = bucket.OrderBy(obj => obj.Key);
+                    foreach(var pair in orderedBucket)
+                    {
+                        results.Add(pair.Key);
+                        if (results.Count >= k)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return results;
+
+        }
+
+        public Dictionary<string, int> CountWordsEntries(string[] words)
+        {
+            var countedWords = new Dictionary<string, int>();
+
+            foreach(var word in words)
+            {
+                int wordCount;
+                if (countedWords.TryGetValue(word, out wordCount))
+                {
+                    wordCount++;
+                    countedWords[word] = wordCount;
+                }
+                else
+                {
+                    countedWords.Add(word, 1);
+                }
+            }
+
+            return countedWords;
+        }
+
+        public List<KeyValuePair<string, int>>[] ThrowToBucket(Dictionary<string, int> countedWords, string[] words)
+        {
+            var bucketsList = new List<KeyValuePair<string, int>>[words.Count()];
+
+            foreach(var pair in countedWords)
+            {
+                var index = pair.Value;
+
+                if (bucketsList[index] == null)
+                {
+                    var bucket = new List<KeyValuePair<string, int>>();
+                    bucket.Add(pair);
+                    bucketsList[index] = bucket;
+                }
+                else 
+                {
+                    bucketsList[index].Add(pair);
+                }               
+            }
+
+            return bucketsList;
+        }
+    }
+}
